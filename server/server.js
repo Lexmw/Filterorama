a710ec17ff1d83300c19642e935d1ef957f3b037
lexmw <lexius.waltar@gmail.com>
2021-09-13 08:57:02 -0700

    transitions project to simple react app, adds the general upload, download functionality, and sets the filters. What still needs to get done is the update of a file once the filter is added, as of now the filter doenst update the original file.

--- server/server.js
+++ server/server.js
@@ -1,43 +1,10 @@
 const express = require('express');
-const multer = require('multer');
 const path = require('path');
 const app = express();
-// const axios = require('axios');
 
-app.use(express.static('dist'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"));
+app.use(express.static("public/assets"));
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/');
-    },
-
-    // By default, multer removes file extensions so let's add them back
-    filename: function(req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-});
-
-const upload = multer({ storage: storage }).single('file')
-
-app.get('/upload', function (req, res) {
-    res.send('hello world')
-  })
-
-app.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-      if (err) {
-        console.log('something went wrong', res ,err)
-        res.sendStatus(500);
-      }
-      res.send(req.file);
-    });
-  });
-
-
-
-app.use(express.static('upload'));
-
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
